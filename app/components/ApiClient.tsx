f787150e1446e0269587e6854405909e64d72da2
krismasdev <superlucky19971023@gmail.com>
2025-06-18 10:41:48 -0400

    deploy

--- app/components/ApiClient.tsx
+++ app/components/ApiClient.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://pdf-backend-pela.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
