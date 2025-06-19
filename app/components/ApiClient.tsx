8380c746d36780c323dd67a28ffc7449f5115481
krismasdev <superlucky19971023@gmail.com>
2025-06-19 03:25:42 -0400

    Update API base URL in ApiClient.tsx

--- app/components/ApiClient.tsx
+++ app/components/ApiClient.tsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pdf-parse-backend-jucpmx4k4-smartguy4112-gmailcoms-projects.vercel.app/",
+  baseURL: "pdf-parse-backend-baqca8qkv-smartguy4112-gmailcoms-projects.vercel.app",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
